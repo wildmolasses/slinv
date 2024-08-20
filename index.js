784703fdc82c181b9543c6548f51750ca3649be0
wildmolasses <changes@gmail.com>
2024-08-20 14:09:24 -0400

    initial

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-export default function unicornFun(input, {postfix = 'rainbows'} = {}) {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+#!/usr/bin/env node
 
-	return `${input} & ${postfix}`;
+import { editor, input } from "@inquirer/prompts";
+
+export default async function main() {
+	const seq = await editor({
+		message: "Enter your invariant call stack",
+	});
+
+	const handler = await input({
+		message: `Enter your handler contract variable name`,
+		default: "handler",
+	});
+
+	const reg =
+		/sender=(0x[0-9a-fA-F]+)\ addr=\[.+calldata=([A-Za-z0-9]+)\(.+args=\[((?:(?:([A-Za-z0-9]+(?:\s\[[0-9\.e]+\])?),?)\s?)*)\]/g;
+
+	const matches = seq.matchAll(reg);
+
+	for (const match of matches) {
+		let [_, caller, fn, args] = match;
+		args = args.replace(/\s?\[.*?\]/g, "");
+		console.log(`${handler}.${fn}(${args});`);
+	}
 }
+
+main();
